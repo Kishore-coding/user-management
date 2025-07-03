91842dcb7ad7f9384fa194e530c4fdeb1d2d1e31
Kishore <kishore.raju@nurtur.tech>
2025-07-03 13:29:34 +0530

    removed console and commentd codes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import UserTable from "./components/UserTable/UserTable";
-// import TableHeader from "./components/TableHeader/TableHeader";
 
 export default function Home() {
   return (
     <main>
-      {/* <TableHeader /> */}
       <UserTable />
     </main>
   );
